9ae4a16c7c26bad190e9e12dde44025c771719c9
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-09-23 15:55:24 +0530

    Feature added.
    Now bot echoes the input message i.e returns the same message.

--- src/com/ruchitesh/gcetianBot.java
+++ src/com/ruchitesh/gcetianBot.java
@@ -1,11 +1,23 @@
 package com.ruchitesh;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class gcetianBot extends TelegramLongPollingBot {
     private final String botToken=giveMeMyToken.getBotToken();
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage()&&update.getMessage().hasText()) {
+            SendMessage replyOfBot = new SendMessage().setChatId(update.getMessage().getChatId());
+            replyOfBot.setText(update.getMessage().getText());
+
+            try {
+                execute(replyOfBot);
+            }catch (TelegramApiException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
