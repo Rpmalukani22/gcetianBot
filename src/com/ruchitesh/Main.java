a97e79443c2926fdd7e17ab6288ac0d3b6de51f0
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-09-23 14:53:11 +0530

    Initial commit

---
+++ src/com/ruchitesh/Main.java
@@ -0,0 +1,8 @@
+package com.ruchitesh;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/ruchitesh/gcetianBot.java
@@ -0,0 +1,21 @@
+package com.ruchitesh;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class gcetianBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
