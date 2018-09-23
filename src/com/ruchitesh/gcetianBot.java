80b8e172fd3cbe69318825dc132b927e2f39875c
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-09-23 15:06:00 +0530

    Inital setup created.
    Bot-token and username are now set.

--- src/com/ruchitesh/gcetianBot.java
+++ src/com/ruchitesh/gcetianBot.java
@@ -1,21 +1,21 @@
 package com.ruchitesh;
-
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class gcetianBot extends TelegramLongPollingBot {
+    private final String botToken=
     @Override
     public void onUpdateReceived(Update update) {
-        
+
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "gcetianBot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 }
